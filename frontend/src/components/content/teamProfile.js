3c25996e170868e1e6f12fc6e31b41d0d6ccd13e
Eduardo Vieira <eduardortodontia@gmail.com>
2018-12-11 14:40:37 -0200

    ...

--- frontend/src/components/content/teamProfile.js
+++ frontend/src/components/content/teamProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 export default class teamProfile extends Component {
   render () {
+    console.log(this.props)
     return (
       <div className='container-fluid bg-primary-shadow mt-4'>
         <div className='row'>
@@ -20,31 +21,31 @@ export default class teamProfile extends Component {
               <li className='nav-item'>
                 <div className='py-3 px-2 align-items-center text-truncate' style={{maxWidth: '150px'}}>
                   <img className='rounded-circle img-thumbnail' src='https://via.placeholder.com/100' />
-                  <p className='text-white pt-2 text-truncate ml-auto'>PARAGATIMIRUARU</p>
+                  <p className='text-white pt-2 text-truncate ml-auto' style={{maxWidth: '100px'}}>lorem ipsum amet</p>
                 </div>
               </li>
               <li className='nav-item'>
                 <div className='py-3 px-2 align-items-center text-truncate' style={{maxWidth: '150px'}}>
                   <img className='rounded-circle img-thumbnail' src='https://via.placeholder.com/100' />
-                  <p className='text-white pt-2 text-truncate ml-auto'>PARAGATIMIRUARU</p>
+                  <p className='text-white pt-2 text-truncate ml-auto' style={{maxWidth: '100px'}}>lorem ipsum amet</p>
                 </div>
               </li>
               <li className='nav-item'>
                 <div className='py-3 px-2 align-items-center text-truncate' style={{maxWidth: '150px'}}>
                   <img className='rounded-circle img-thumbnail' src='https://via.placeholder.com/100' />
-                  <p className='text-white pt-2 text-truncate ml-auto'>PARAGATIMIRUARU</p>
+                  <p className='text-white pt-2 text-truncate ml-auto' style={{maxWidth: '100px'}}>lorem ipsum amet</p>
                 </div>
               </li>
               <li className='nav-item'>
                 <div className='py-3 px-2 align-items-center text-truncate' style={{maxWidth: '150px'}}>
                   <img className='rounded-circle img-thumbnail' src='https://via.placeholder.com/100' />
-                  <p className='text-white pt-2 text-truncate ml-auto'>PARAGATIMIRUARU</p>
+                  <p className='text-white pt-2 text-truncate mr-auto ml-auto' style={{maxWidth: '100px'}}>lorem ipsum amet</p>
                 </div>
               </li>
               <li className='nav-item'>
                 <div className='py-3 px-2 align-items-center text-truncate' style={{maxWidth: '150px'}}>
                   <img className='rounded-circle img-thumbnail' src='https://via.placeholder.com/100' />
-                  <p className='text-white pt-2 text-truncate ml-auto'>PARAGATIMIRUARU</p>
+                  <p className='text-white pt-2 text-truncate ml-auto mr-auto' style={{maxWidth: '100px'}}>lorem ipsum amet</p>
                 </div>
               </li>
             </ul>
