3ebbe8213b97c5336cfb6ab117ddd61021811a21
Eduardo <eduardortodontia@gmail.com>
2018-12-13 20:56:19 -0200

    ...

--- frontend/src/components/content/teamProfile.js
+++ frontend/src/components/content/teamProfile.js
@@ -4,11 +4,25 @@ import { Redirect } from 'react-router-dom'
 
 import Loading from './teamProfile/loading'
 import { getTeamData } from '../../redux/actions/team'
+import axios from '../../axios-public'
 
 class TeamProfile extends Component {
+  state = {
+    teamId: null
+  }
   componentDidMount () {
     const { id } = this.props.match.params
     this.props.getTeamData(id)
+    this.setState({teamId: id})
+  }
+  enterTeam = () => {
+    const { teamId } = this.props.match.params
+    console.log(teamId)
+    const data = {
+      teamId: this.state.teamId,
+      teamPassword: '130190'
+    }
+    axios.put('/team/join', data).then((data) => console.log(data)).catch(err => console.log(err.message))
   }
   render () {
     console.log(this.props.data)
@@ -18,7 +32,7 @@ class TeamProfile extends Component {
       const players = this.props.players.map((player) => {
         return (
           <li className='nav-item' key={player._id}>
-            <div className='py-3 px-2 align-items-center text-truncate' style={{maxWidth: '150px'}}>
+            <div className='py-3 px-2 align-items-center text-truncate' style={{maxWidth: '100px'}}>
               <img className='rounded-circle img-thumbnail' src={player.avatarfull} />
               <p className='text-white pt-2 text-truncate ml-auto' >{player.nickname}</p>
             </div>
@@ -29,7 +43,8 @@ class TeamProfile extends Component {
         <div className='container-fluid bg-primary-shadow mt-4' style={{height: '77vh'}}>
           <div className='row'>
             <div className='col bg-primary-shadow d-flex align-items-center justify-content-center' style={{height: '180px'}}>
-              <img className='rounded-circle img-fluid img-thumbnail ' src='https://via.placeholder.com/150' />
+              <img className='rounded-circle img-fluid img-thumbnail' src='https://via.placeholder.com/100' />
+              <button className='btn btn-success ml-3' onClick={this.enterTeam}>Entrar</button>
             </div>
           </div>
           <div className='row'>
